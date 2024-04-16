1ff29b30f886dd299aa054c6f9addf0f21f205ec
Abdulrahman Ahmed <bedo150@gmail.com>
2024-04-16 09:39:09 +0200

    Javascript Nuggets - Destructuring (object)

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,6 @@
+// Javascript Nuggets - Destructuring (object)
+// url = https://youtu.be/i4vhNKihfto?list=PLnHJACx3NwAfRUcuKaYhZ6T5NRIpzgNGJ
+
 const Max = {
   first: "max",
   last: "well",
@@ -8,4 +11,21 @@ const Max = {
   },
 };
 
-console.log(Max);
+const {
+  first: firstName,
+  last: lastName,
+  age,
+  jobs: { first: firstJob },
+} = Max;
+console.log(firstName, lastName, age, firstJob);
+
+function printMax({ first, last, age, jobs: { first: firstJob, last: lastJob } }) {
+  console.log(
+    `my first name is : ${first},
+    my last name is : ${last},
+    my age is : ${age},
+    my first job is ${firstJob},
+    my last job is ${lastJob}`
+  );
+}
+printMax(Max);
