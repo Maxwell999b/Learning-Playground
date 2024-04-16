4564fade2e9f81355f0fe7283c3fa1936ad0aeed
Abdulrahman Ahmed <bedo150@gmail.com>
2024-04-16 09:07:26 +0200

    Remove submodule relationship and track directories directly

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+const Max = {
+  first: "max",
+  last: "well",
+  age: 25,
+  jobs: {
+    first: "Engineer",
+    last: "Programmer",
+  },
+};
+
+console.log(Max);
---
+++ React/my-app/src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const firstBook = {
+  img: "./images/book1.jpg",
+  title: "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+  author: "James Clear",
+};
+
+const secondBook = {
+  img: "./images/book2.jpg",
+  title: "The Housemaid Paperback",
+  author: "Freida McFadden",
+};
+
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book author={firstBook.author} title={firstBook.title} img={firstBook.img} />
+      <Book author={secondBook.author} title={secondBook.title} img={secondBook.img} />
+    </section>
+  );
+}
+
+const Book = (props) => {
+  const { img, title, author } = props;
+  return (
+    <article className="book">
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+    </article>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
