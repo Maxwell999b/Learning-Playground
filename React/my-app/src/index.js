6ec12feab5ee5447bce4435b5a625e7b5f6882d3
Abdulrahman Ahmed <bedo150@gmail.com>
2024-04-18 18:51:45 +0200

    Events - Fundamentals

--- React/my-app/src/index.js
+++ React/my-app/src/index.js
@@ -20,13 +20,55 @@ const books = [
 function BookList() {
   return (
     <section className="bookList">
+      <EventExample />
       {books.map((book) => {
         return <Book {...book} key={book} />;
       })}
     </section>
   );
 }
-// comment
+
+const EventExample = () => {
+  const handleStyle = {
+    margin: "5px",
+    padding: "5px",
+    borderRadius: "5px",
+    border: "2px solid black",
+    backgroundColor: "white",
+    color: "black",
+    fontSize: "20px",
+    fontWeight: "bold",
+    textAlign: "center",
+    textDecoration: "none",
+    width: "300px",
+    height: "50px",
+  };
+  const handleInput = () => {
+    console.log("handle_Input_LOG");
+  };
+  const handleButton = () => {
+    console.log("handle_Button_LOG");
+  };
+  return (
+    <div>
+      <form>
+        <h1>Hello</h1>
+        <input
+          type="text"
+          name="inputName"
+          placeholder="Enter your name"
+          style={{ handleStyle }}
+          onChange={handleInput}
+        />
+
+        <button type="button" style={handleStyle} onClick={handleButton}>
+          ClickMe
+        </button>
+      </form>
+    </div>
+  );
+};
+
 const Book = (props) => {
   const { img, title, author } = props;
   return (
