c5708a0ab9fe56e509cc97b465cdf9d28dcd2beb
Abdulrahman Ahmed <bedo150@gmail.com>
2024-04-24 02:37:56 +0200

    More Complex Example & Import and Export Statements another approach #2

--- React/my-app/src/Book.js
+++ React/my-app/src/Book.js
@@ -1,15 +1,12 @@
 const Book = (props) => {
   const { img, title, author, getBooks, id } = props;
   //   console.log(props);
-  const getSingleBook = () => {
-    getBooks(id);
-  };
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      <button type="button" onClick={getSingleBook}>
+      <button type="button" onClick={() => getBooks(id)}>
         Show Title
       </button>
     </article>
