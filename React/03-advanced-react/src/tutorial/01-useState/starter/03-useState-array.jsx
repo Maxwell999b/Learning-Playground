55d26c3d1708d29bfee744e8dcd1eb04ce5f258e
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-04 00:14:05 +0300

    usestate re-try

--- React/03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ React/03-advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import { useState } from "react";
 import { data } from "../../../data";
 const UseStateArray = () => {
-  const [people, setPeople] = React.useState(data);
+  const [people, setPeople] = useState(data);
   const removeItem = (id) => {
     let newPeople = people.filter((person) => person.id !== id);
     setPeople(newPeople);
@@ -11,16 +11,16 @@ const UseStateArray = () => {
       {people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id} className="item">
+          <div key={id}>
             <h4>{name}</h4>
-            <button type="button" style={{ backgroundColor: "skyblue" }} onClick={() => removeItem(id)}>
+            <button type="button" onClick={() => removeItem(id)}>
               Delete
             </button>
           </div>
         );
       })}
-      <button className="btn" type="button" style={{ marginTop: "2rem" }} onClick={() => setPeople([])}>
-        Clear All
+      <button type="button" className="btn" style={{ marginTop: "2rem" }} onClick={() => setPeople([])}>
+        ClearAll
       </button>
     </div>
   );
