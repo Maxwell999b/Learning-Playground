21e9040796041432e7550db2b1b358916f463161
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-04 02:13:34 +0300

    usestate gotcha with latest update on the number

--- React/03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ React/03-advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -2,12 +2,16 @@ import { useState } from "react";
 const UseStateGotcha = () => {
   const [value, setValue] = useState(0);
   const updateValue = () => {
-    setValue(value + 1);
-    console.log(value);
+    setValue((prevState) => {
+      const newState = prevState + 1;
+      console.log(newState);
+      return newState;
+    });
+    // console.log("old : " + value);
   };
   return (
     <>
-      <h2>{value}</h2>
+      <h2>{`You have Clicked: ${value}`}</h2>
       <button type="button" onClick={updateValue} className="btn">
         update Number
       </button>
