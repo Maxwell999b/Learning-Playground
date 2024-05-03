59077d5b824c4f367e88fec2670d6df9e620ba70
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-04 01:03:56 +0300

    useState object

--- React/03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ React/03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,23 @@
-import { useState } from 'react';
-
+import { useState } from "react";
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState("Max");
+  const [age, setAge] = useState(21);
+  const [hobby, setHobby] = useState("read books");
+  const UpdateStateObject = () => {
+    setName("John");
+    setAge(28);
+    setHobby("scream at the computer");
+  };
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{age}</h2>
+      <h3>{hobby}</h3>
+      <button type="button" onClick={UpdateStateObject} className="btn">
+        change state
+      </button>
+    </div>
+  );
 };
 
 export default UseStateObject;
