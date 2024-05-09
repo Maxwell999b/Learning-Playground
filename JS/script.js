7d135934c3761a76af43cd2e60645827288d891b
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-10 01:39:37 +0300

    Async js

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,16 @@
-//
-//
+// 13-callback-function
+// https://youtu.be/GWq0XETTOTk?list=PLnHJACx3NwAfRUcuKaYhZ6T5NRIpzgNGJ
+
+function orderPizza(callback) {
+  setTimeout(() => {
+    const pizza = "🍕";
+    callback(pizza);
+    console.log("the pizza is Ready");
+  }, 2000);
+}
+function pizzaReady(pizza) {
+  console.log(`Eat the ${pizza}`);
+  console.log("Call the someoneelse");
+}
+orderPizza(pizzaReady);
+console.log("Call the cli");
