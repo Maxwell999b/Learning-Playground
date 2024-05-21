f25c16bfce46c2ca46ecb3d3fa7e4fbfeac6fe63
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-21 13:16:32 +0300

    republish

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,57 @@
 // 13-callback-function
 // https://youtu.be/GWq0XETTOTk?list=PLnHJACx3NwAfRUcuKaYhZ6T5NRIpzgNGJ
 
-function orderPizza(callback) {
+// let weather;
+// console.log(weather);
+getWeather(weatherReceived);
+getWeather(displayIcon);
+// document.body.innerText = weather;
+function displayIcon(data) {
+  let icon;
+  if (data === "sunny") {
+    icon = "☀️";
+  } else if (data === "cloudy") {
+    icon = "☁️";
+  }
+  document.querySelector(".my-input-result").value = "Weather: " + data + icon;
+}
+
+function weatherReceived(data) {
+  let weather = data;
+  displayIcon(weather);
+}
+
+function getWeather(callback) {
   setTimeout(() => {
-    const pizza = "🍕";
-    callback(pizza);
-    console.log("the pizza is Ready");
-  }, 2000);
+    callback("sunny");
+  });
 }
-function pizzaReady(pizza) {
-  console.log(`Eat the ${pizza}`);
-  console.log("Call the someoneelse");
-}
-orderPizza(pizzaReady);
-console.log("Call the cli");
+
+getWeather(weatherReceived);
+
+// function getWeather() {
+//   setTimeout(() => {
+//     weather = "sunny";
+//     document.body.innerText = weather;
+//   });
+// }
+
+// function getWeather() {
+//   return "sunny";
+// }
+
+// function getWeather() {
+//   setTimeout(() => {
+//     return "sunny"
+//   })
+// }
+
+// const btn = document.getElementsByClassName("btn")[0];
+// btn.addEventListener("click", function () {
+//   console.log("Hello World");
+// });
+
+// const btn = document.querySelector(".btn");
+// btn.addEventListener("click", function () {
+//   console.log("Hello World");
+// });
