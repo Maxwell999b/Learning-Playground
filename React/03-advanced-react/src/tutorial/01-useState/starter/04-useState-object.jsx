67dbb3ef6b5ff329edeb15c6fc87e0b3446e0eed
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-22 06:37:41 +0300

    update 04-useState-object.jsx

--- React/03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ React/03-advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,23 +1,24 @@
 import { useState } from "react";
 
 const UseStateObject = () => {
-  let [name, setName] = useState("Peter");
-  const [age, setAge] = useState(24);
-  const [hobby, setHobby] = useState("Read Books");
+  let [person, setPerson] = useState({ name: "peter", age: 24, hobby: "Read Books" });
+  const { name, age, hobby } = person;
   const handleButton = () => {
-    setName("John");
-    setAge(28);
-    setHobby("Screaming At The Computer");
+    setPerson({
+      name: "John",
+      age: 28,
+      hobby: "Screaming At The Computer",
+    });
   };
   return (
-    <div>
+    <>
       <h1>{name}</h1>
       <h2>{age}</h2>
       <h4>{hobby}</h4>
       <button className="btn" onClick={handleButton}>
         Change the Data
       </button>
-    </div>
+    </>
   );
 };
 
