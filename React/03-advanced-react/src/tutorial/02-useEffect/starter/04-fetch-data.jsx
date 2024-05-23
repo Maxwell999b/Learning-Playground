daf3c4068f7d81854f9a333957ab94a1febcb8a6
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-23 10:07:57 +0300

    update 04-fetch-data.jsx

--- React/03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
+++ React/03-advanced-react/src/tutorial/02-useEffect/starter/04-fetch-data.jsx
@@ -1,6 +1,43 @@
-const url = 'https://api.github.com/users';
+import { useState, useEffect } from "react";
+
+const url = "https://api.github.com/users";
 
 const FetchData = () => {
-  return <h2>fetch data example</h2>;
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    const fetchingFunc = async () => {
+      try {
+        const resp = await fetch(url);
+        if (!resp.ok) {
+          const msg = `Try Block : ${resp.status}  ${resp.statusText} `;
+          throw new Error(msg);
+        }
+        const data = await resp.json();
+        setUsers(data);
+      } catch (error) {
+        console.log(`Error from the Catch Block : ${error}`);
+      }
+    };
+    fetchingFunc();
+  }, []);
+  return (
+    <>
+      <h3>github users</h3>
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h5>{login}</h5>
+                <a href={html_url}>Profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 export default FetchData;
