c22955fef5f7b30a9e0e6c31de3b159670d36891
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-23 06:03:09 +0300

    update App.jsx and 02-useEffect-basics.jsx log every initial render

--- React/03-advanced-react/src/App.jsx
+++ React/03-advanced-react/src/App.jsx
@@ -1,10 +1,10 @@
-import Starter from "./tutorial/02-useEffect/starter/01-code-example.jsx";
+import Starter from "./tutorial/02-useEffect/starter/02-useEffect-basics.jsx";
 import Final from "./tutorial/02-useEffect/final/01-code-example.jsx";
 function App() {
   return (
     <div className="container">
-      {/* <Starter /> */}
-      <Final />
+      <Starter />
+      {/* <Final /> */}
     </div>
   );
 }
