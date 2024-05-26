388abd63b5bf47cfebfa27f0285532e025ac0996
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-26 15:08:11 +0300

    update 07-user-challenge.jsx

--- React/03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ React/03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -2,36 +2,34 @@ import { useState } from "react";
 
 const UserChallenge = () => {
   const [user, setUser] = useState(null);
+
   function login() {
     setUser({ name: "Maxwell999" });
   }
+
   function logout() {
     setUser(null);
   }
-  // function toggleUser() {
-  //   setUser((prevUser) => (prevUser ? null : { name: "Maxwell999" }));
-  // }
-  return <div>{user ? <LogoutForm user={user} logout={logout} /> : <LoginForm login={login} />}</div>;
+
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h3>Hello there, {user.name}</h3>
+          <button className="btn" onClick={logout}>
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h3>Please Login</h3>
+          <button className="btn" onClick={login}>
+            Login
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
-const LogoutForm = ({ user, logout }) => {
-  return (
-    <div>
-      <h3>Hello there, {user.name}</h3>
-      <button className="btn" onClick={() => logout()}>
-        Logout
-      </button>
-    </div>
-  );
-};
-const LoginForm = ({ login }) => {
-  return (
-    <div>
-      <h3>Please Login</h3>
-      <button className="btn" onClick={() => login()}>
-        Login
-      </button>
-    </div>
-  );
-};
 export default UserChallenge;
