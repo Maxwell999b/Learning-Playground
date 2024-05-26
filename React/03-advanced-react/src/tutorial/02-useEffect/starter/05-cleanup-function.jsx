3f6a3131e57e0da6d7c90af195e08a2184602fc1
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-26 15:36:55 +0300

    update App.jsx and 05-cleanup-function.jsx

--- React/03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ React/03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,21 @@
+import { useState, useEffect } from "react";
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [show, setShow] = useState(false);
+
+  return (
+    <div>
+      <button className="btn" onClick={() => setShow(!show)}>
+        Toggle
+      </button>
+      {show && <ShowFirst />}
+    </div>
+  );
+};
+const ShowFirst = () => {
+  useEffect(() => {
+    console.log("toggle is there...");
+  }, []);
+  return <h1>Show Text Toggling..</h1>;
 };
 
 export default CleanupFunction;
