318a29259060bae7e5e4c09fcfe1404d83ea0d27
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-26 09:22:13 +0300

    update 02-multiple-returns-fetch-data.jsx

--- React/03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ React/03-advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -2,20 +2,39 @@ import { useEffect, useState } from "react";
 const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
-  const [user, setUser] = useState([]);
-  // const [user, setUser] = useState(null);
+  // const [user, setUser] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState(null);
+
   useEffect(() => {
     const Fetching = async () => {
-      const response = await fetch(url);
-      // console.log(response);
-      const user = await response.json();
-      console.log(user);
-      setUser(user);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          setIsError(true);
+          setIsLoading(false);
+          const msg = "There was an error 400-500";
+          throw new Error(msg);
+        }
+        // console.log(response);
+        const user = await response.json();
+        // console.log(user);
+        setUser(user);
+      } catch (error) {
+        setIsError(true);
+        console.log(error);
+      }
+      setIsLoading(false);
     };
     Fetching();
   }, []);
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>There was an error...</h2>;
+  }
   return (
     <div>
       <img style={{ width: "250px", borderRadius: "25px" }} src={user.avatar_url} alt={user.name} />
