382e30c65d566b8974ebb5d0ad3192b6088a6fd1
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-26 14:55:14 +0300

    update App.jsx and 07-user-challenge.jsx

--- React/03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ React/03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
 const UserChallenge = () => {
-  return <h2>user challenge</h2>;
+  const [user, setUser] = useState(null);
+  function login() {
+    setUser({ name: "Maxwell999" });
+  }
+  function logout() {
+    setUser(null);
+  }
+  function toggleUser() {
+    setUser((prevUser) => (prevUser ? null : { name: "Maxwell999" }));
+  }
+  return (
+    <div>
+      {user ? (
+        <div>
+          <h1>Hello there {user.name}</h1>
+          <button className="btn" onClick={() => toggleUser()}>
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <h4>Please Login</h4>
+          <button className="btn" onClick={() => toggleUser()}>
+            Login
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default UserChallenge;
