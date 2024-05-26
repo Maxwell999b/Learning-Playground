0d32b573e0cc3d0424651418fabe72a4282488cd
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-26 14:55:27 +0300

    update 07-user-challenge.jsx

--- React/03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
+++ React/03-advanced-react/src/tutorial/03-conditional-rendering/starter/07-user-challenge.jsx
@@ -8,22 +8,22 @@ const UserChallenge = () => {
   function logout() {
     setUser(null);
   }
-  function toggleUser() {
-    setUser((prevUser) => (prevUser ? null : { name: "Maxwell999" }));
-  }
+  // function toggleUser() {
+  //   setUser((prevUser) => (prevUser ? null : { name: "Maxwell999" }));
+  // }
   return (
     <div>
       {user ? (
         <div>
           <h1>Hello there {user.name}</h1>
-          <button className="btn" onClick={() => toggleUser()}>
+          <button className="btn" onClick={() => login()}>
             Logout
           </button>
         </div>
       ) : (
         <div>
           <h4>Please Login</h4>
-          <button className="btn" onClick={() => toggleUser()}>
+          <button className="btn" onClick={() => logout()}>
             Login
           </button>
         </div>
