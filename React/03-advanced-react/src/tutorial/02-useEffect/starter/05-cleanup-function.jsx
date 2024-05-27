5f43e8d1ef4972babd0b04b8704df7132d96865a
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-27 22:11:48 +0300

    update 05-cleanup-function.jsx

--- React/03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ React/03-advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 const CleanupFunction = () => {
   const [show, setShow] = useState(false);
-
+  // console.log("render & re-render");
   return (
     <div>
       <button className="btn" onClick={() => setShow(!show)}>
@@ -13,13 +13,22 @@ const CleanupFunction = () => {
 };
 const ShowFirst = () => {
   useEffect(() => {
-    // console.log('hmm, this is interesting');
-    const intID = setInterval(() => {
-      console.log("hello from interval");
-    }, 1000);
-    // does not stop, keeps going
-    // every time we render component new interval gets created
-    return () => clearInterval(intID);
+    // console.log("hmm, this is interesting");
+    // const intID = setInterval(() => {
+    //   console.log("hello from interval");
+    // }, 1000);
+    // // does not stop, keeps going
+    // // every time we render component new interval gets created
+    // return () => {
+    //   clearInterval(intID);
+    //   console.log("cleanup");
+    // };
+    const someFunc = () => {
+      // some logic here
+      console.log("someFunc logged");
+    };
+    window.addEventListener("scroll", someFunc);
+    return () => window.removeEventListener("scroll", someFunc);
   }, []);
   return <h1>hello there</h1>;
 };
