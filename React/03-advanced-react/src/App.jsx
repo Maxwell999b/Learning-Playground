04fc245d8ed523cb74d68fdcb57d835de9e7c3c7
Abdulrahman Ahmed <bedo150@gmail.com>
2024-05-28 15:06:56 +0300

    update App.jsx

--- React/03-advanced-react/src/App.jsx
+++ React/03-advanced-react/src/App.jsx
@@ -1,12 +1,5 @@
-import Starter from "./tutorial/02-useEffect/starter/05-cleanup-function.jsx";
-import Final from "./tutorial/02-useEffect/final/05-cleanup-function.jsx";
 function App() {
-  return (
-    <div className="container">
-      <Starter />
-      {/* <Final /> */}
-    </div>
-  );
+  return <div className="container"></div>;
 }
 
 export default App;
