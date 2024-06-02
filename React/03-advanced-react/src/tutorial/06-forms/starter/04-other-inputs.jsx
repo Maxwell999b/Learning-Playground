0736b974c3eaae9cbc42cb02c12d9323b0c808a4
Abdulrahman Ahmed <bedo150@gmail.com>
2024-06-02 16:58:26 +0300

    update 04-other-inputs.jsx

--- React/03-advanced-react/src/tutorial/06-forms/starter/04-other-inputs.jsx
+++ React/03-advanced-react/src/tutorial/06-forms/starter/04-other-inputs.jsx
@@ -7,13 +7,16 @@ const OtherInputs = () => {
     console.log(e.target.checked);
     setShipping(e.target.checked);
   };
-  const handleFramework = (e) => {
-    setFramework(e.target.value);
-  };
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(shipping);
     console.log(framework);
   };
+  const handleFramework = (e) => {
+    console.log(e.target.value);
+    setFramework(e.target.value);
+  };
+
   return (
     <div>
       <form className="form" onSubmit={handleSubmit}>
@@ -21,13 +24,15 @@ const OtherInputs = () => {
         {/* name */}
         <div className="form-row" style={{ textAlign: "left" }}>
           <label htmlFor="shipping"> Free Shipping </label>
-          <input type="checkbox" checked={shipping} id="shipping" name="shipping" onChange={handleShipping} />
+          <input type="checkbox" name="shipping" id="shipping" checked={shipping} onChange={handleShipping} />
         </div>
         <div className="form-row" style={{ textAlign: "left" }}>
           <label htmlFor="framework" className="form-label">
             Framework
           </label>
           <select name="framework" id="framework" value={framework} onChange={handleFramework}>
+            {framework}
+
             {frameworks.map((framework) => {
               return <option key={framework}>{framework}</option>;
             })}
