95007d1b1b7bea06839dc2d51026e7454c8f7368
Abdulrahman Ahmed <bedo150@gmail.com>
2024-06-03 17:31:21 +0300

    update App.jsx and 02-fetch-data.jsx

--- React/03-advanced-react/src/App.jsx
+++ React/03-advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from "./tutorial/08-custom-hooks/starter/01-toggle.jsx";
-import Final from "./tutorial/06-forms/final/05-form-data.jsx";
+import Starter from "./tutorial/08-custom-hooks/starter/02-fetch-data.jsx";
+import Final from "./tutorial/08-custom-hooks/final/02-fetch-data.jsx";
 function App() {
   return (
     <div className="container">
