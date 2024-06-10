1b5cee98ae27e94142807f444925fc58e76f42bd
Abdulrahman Ahmed <bedo150@gmail.com>
2024-06-11 00:14:24 +0300

    update App.jsx and context.jsx

--- React/global-context/src/context.jsx
+++ React/global-context/src/context.jsx
@@ -1,10 +1,10 @@
 import { createContext, useContext, useState } from "react";
-
 const GlobalContext = createContext();
+
 export const useGlobalContext = () => useContext(GlobalContext);
+
 const AppContext = ({ children }) => {
-  const [name, setName] = useState("Maxwell");
+  const [name, setName] = useState("Maxwell999b");
   return <GlobalContext.Provider value={{ name, setName }}>{children}</GlobalContext.Provider>;
 };
-
 export default AppContext;
