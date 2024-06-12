10456c0d44c8c41df680a976c0af037c6995ea62
Abdulrahman Ahmed <bedo150@gmail.com>
2024-06-13 00:39:16 +0300

    update App.jsx, List.jsx and React/03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx

--- React/03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx
+++ React/03-advanced-react/src/tutorial/11-performance/starter/03-hooks/index.jsx
@@ -1,17 +1,16 @@
-import { useState } from 'react';
-import { data } from '../../../../data';
-import List from './List';
+import { useState } from "react";
+import { data } from "../../../../data";
+import List from "./List";
 const LowerState = () => {
   const [people, setPeople] = useState(data);
   const [count, setCount] = useState(0);
-
+  const removePerson = (id) => {
+    const newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
   return (
     <section>
-      <button
-        className='btn'
-        onClick={() => setCount(count + 1)}
-        style={{ marginBottom: '1rem' }}
-      >
+      <button className="btn" onClick={() => setCount(count + 1)} style={{ marginBottom: "1rem" }}>
         count {count}
       </button>
       <List people={people} />
