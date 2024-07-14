6457a7f0faaf8489e2b1db333be0fef854c74e35
Abdulrahman Ahmed <bedo150@gmail.com>
2024-07-14 18:08:31 +0300

    update Nodejs/index.js

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -1,1 +1,7 @@
 // node js learning area start 13-07-2024
+console.log("Hello World");
+const loggers = require("./logger");
+// loggers.log1();
+// loggers.log2();
+console.log(loggers);
+// console.log(`${loggers} + ${loggers.log1()} + ${loggers.log2()}`);
---
+++ Nodejs/logger.js
@@ -0,0 +1,16 @@
+console.log("this is from logger js");
+
+function log() {
+  console.log("this is from log0");
+}
+function log1() {
+  console.log("this is from log1");
+}
+function log2() {
+  console.log("this is from log2");
+}
+
+module.exports = {
+  log1,
+  log2,
+};
