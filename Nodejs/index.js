38b9d14b2d48ec1b8dd054817ad8660dbddbee2d
Abdulrahman Ahmed <bedo150@gmail.com>
2024-07-17 21:41:54 +0300

    update Nodejs/index.js

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -7,7 +7,7 @@
 // console.log(loggers);
 // // console.log(`${loggers} + ${loggers.log1()} + ${loggers.log2()}`);
 const fs = require("fs");
-
+const http = require("http");
 // Blocking synchronous way
 // const textIn = fs.readFileSync("./1-node-farm/starter/txt/input.txt", "utf-8");
 // console.log(textIn);
@@ -19,23 +19,46 @@ const fs = require("fs");
 
 // Non-Blocking asynchronous way
 
-fs.readFile("./1-node-farm/starter/txt/start.txt", "utf-8", (error, data1) => {
-  if (error) return console.log("ERROR#1 💥");
-  fs.readFile(`./1-node-farm/starter/txt/${data1}.txt`, "utf-8", (error, data2) => {
-    if (error) return console.log("ERROR#2 💥");
-    console.log(`#2 :${data2}`);
-    fs.readFile("./1-node-farm/starter/txt/append.txt", "utf-8", (error, data3) => {
-      if (error) return console.log("ERROR#3 💥");
-      console.log(`#3 :${data3}`);
-      fs.writeFile("./1-node-farm/starter/txt/final.txt", `${data2}\n${data3}`, "utf-8", (error) => {
-        if (error) return console.log("ERROR#4 💥");
-        console.log("the file has been written 😁");
-        fs.readFile("./1-node-farm/starter/txt/final.txt", "utf-8", (error, data4) => {
-          if (error) return console.log("ERROR#5 💥");
-          console.log(`#4 :${data4}`);
-        });
-      });
-    });
-  });
+// fs.readFile("./1-node-farm/starter/txt/start.txt", "utf-8", (error, data1) => {
+//   if (error) return console.log("ERROR#1 💥");
+//   fs.readFile(`./1-node-farm/starter/txt/${data1}.txt`, "utf-8", (error, data2) => {
+//     if (error) return console.log("ERROR#2 💥");
+//     console.log(`#2 :${data2}`);
+//     fs.readFile("./1-node-farm/starter/txt/append.txt", "utf-8", (error, data3) => {
+//       if (error) return console.log("ERROR#3 💥");
+//       console.log(`#3 :${data3}`);
+//       fs.writeFile("./1-node-farm/starter/txt/final.txt", `${data2}\n${data3}`, "utf-8", (error) => {
+//         if (error) return console.log("ERROR#4 💥");
+//         console.log("the file has been written 😁");
+//         fs.readFile("./1-node-farm/starter/txt/final.txt", "utf-8", (error, data4) => {
+//           if (error) return console.log("ERROR#5 💥");
+//           console.log(`#4 :${data4}`);
+//         });
+//       });
+//     });
+//   });
+// });
+// console.log("the end of the Line. 🔚");
+
+/////////////////////////////////////////////////////////////////////////////
+// http server
+const server = http.createServer((req, res) => {
+  res.end("Hello from the Local HTTP Server");
 });
-console.log("the end of the Line. 🔚");
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests at 127.0.0.1, Port: 8000");
+});
+
+// blue link url
+
+// server.listen(8000, "127.0.0.1", () => {
+//   const url = "http://127.0.0.1:8000";
+//   console.log(`Listening to requests at \x1b[34m\x1b[4m${url}\x1b[0m`);
+// });
+
+// link url
+// server.listen(8000, "127.0.0.1", () => {
+//   const url = "http://127.0.0.1:8000";
+//   console.log(`Listening to requests at ${url}`);
+// });
